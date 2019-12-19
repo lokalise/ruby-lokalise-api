9433a427f1a1705ec058648d2cccb4243fd43df9
bodrovis <bodrovis@protonmail.com>
2019-12-19 16:52:14 +0300

    minor updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ end
 require 'ruby-lokalise-api'
 
 # Support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.include TestClient
